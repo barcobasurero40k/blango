831525843cd46dc3e8d09fc4564429a1e8d266dc
marcos B <marcos.ielect@gmail.com>
2022-08-28 13:55:02 +0000

    Finish custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    #posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.all()
     context = {"posts": posts}
     return render(request, "blog/index.html", context)
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug = slug)
+    context = {"post": post}
+    return render(request, "blog/post-detail.html", context)
+
